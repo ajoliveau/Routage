cdcc0578b0e83c183d3972c4b9516850d13b49d2
Arthur <ashenke@gmail.com>
2014-10-19 16:53:47 -0400

    Suppression d'un println qui écrivait dans la console. Tout fonctionne, même en ligne de commande !

--- src/routage/Routage.java
+++ src/routage/Routage.java
@@ -6,6 +6,7 @@ public class Routage {
     private static int nbLiaisons;
     private static int commande;
     private static int[][] matrice;
+    private static int[][] 
    
     public static void main(String[] args) {
         lectureEntree();
@@ -17,13 +18,16 @@ public class Routage {
             case 1:
                 adjacence();
                 break;
+            case 2:
+                distance();
+                break;
         }
     }
     
-    public static void lectureEntree()
-    {
+    public static void lectureEntree() {
         int liaison1;
         int liaison2;
+        int 
         nbRouteurs = Pep8.deci();
         nbLiaisons = Pep8.deci();
         matrice = new int [nbRouteurs][nbRouteurs];
@@ -37,10 +41,13 @@ public class Routage {
         }
         
         commande = Pep8.deci();
+        
+        if (commande == 3) {
+            while 
+        }
     }
     
-    public static void comptage()
-    {
+    public static void comptage() {
         Pep8.stro("n=");
         Pep8.deco(nbRouteurs);
         Pep8.stro("\nm=");
@@ -48,21 +55,59 @@ public class Routage {
         Pep8.charo('\n');
     }
     
-    public static void adjacence()
-    {
+    public static void adjacence() {
+        afficherMatrice();
+    }
+    
+    public static void distance() {
+        
+        formaterMatrice();
+        
+        for (int k = 0; k < nbRouteurs; k++)
+        {
+            for (int i = 0; i < nbRouteurs; i++)
+            {
+                for (int j = 0; j < nbRouteurs; j++)
+                {
+                    if (matrice[i][k] != Integer.MAX_VALUE/2 && matrice[k][j] != Integer.MAX_VALUE/2)
+                    {
+                        if (matrice[i][j] > matrice[i][k] + matrice[k][j]) {
+                            matrice[i][j] = matrice[i][k] + matrice[k][j];
+                        }
+                    }
+                }
+            }
+        }
+        afficherMatrice();
+    }
+    
+    public static void afficherMatrice() {
+        Pep8.charo('\n');
         for (int i=0;i<nbRouteurs;i++)
         {
             for (int j=0;j<nbRouteurs;j++)
             {
-                Pep8.deco(matrice[i][j]);
+                if (matrice[i][j] != Integer.MAX_VALUE/2)
+                    Pep8.deco(matrice[i][j]);
+                else
+                    Pep8.charo('x');
                 Pep8.charo(' ');
             }
             Pep8.charo('\n');
         }
     }
-    
-    public static void distance()
-    {
-        
+    public static void formaterMatrice() {
+        for (int i=0;i<nbRouteurs;i++)
+        {
+            for (int j=0;j<nbRouteurs;j++)
+            {
+                if (i != j && matrice[i][j] == 0)
+                {
+                    matrice[i][j] = Integer.MAX_VALUE/2;
+                }
+            }
+        }
     }
+ 
+   
 }
